0e535aa5e68fc9ab5a0756c824d1e87bd66eeb41
andrashann <andrashann@users.noreply.github.com>
2023-10-12 22:07:49 +0200

    added further layers of protected areas

--- gm_others.js
+++ gm_others.js
@@ -10,6 +10,42 @@ overlayMaps["Védett területek"] = L.tileLayer.wms(
     transparent: true,
     format: "image/png",
     opacity: 0.7,
+    attribution:
+      'Védettség &copy; <a href="https://web.okir.hu/hu/tart/index/234/Interaktiv_termeszetvedelmi_terkep">OKIR</a>',
+  }
+);
+overlayMaps["Lápok és szikes tavak"] = L.tileLayer.wms(
+  "https://webgis.okir.hu/geoserver/tirweb/wms?",
+  {
+    layers: "TI_OW_VE_SZIKES,TI_OW_VE_LAP",
+    transparent: true,
+    format: "image/png",
+    opacity: 0.7,
+    attribution:
+      'Szikes tavak és lápok &copy; <a href="https://web.okir.hu/hu/tart/index/234/Interaktiv_termeszetvedelmi_terkep">OKIR</a>',
+  }
+);
+overlayMaps["Natura2000"] = L.tileLayer.wms(
+  "https://webgis.okir.hu/geoserver/tirweb/wms?",
+  {
+    layers: "natura2000_sac_2017",
+    transparent: true,
+    format: "image/png",
+    opacity: 0.7,
+    attribution:
+      'Natura 2000 &copy; <a href="https://web.okir.hu/hu/tart/index/234/Interaktiv_termeszetvedelmi_terkep">OKIR</a>',
+  }
+);
+overlayMaps["Erdők védettsége"] = L.tileLayer.wms(
+  "https://erdoterkep.nebih.gov.hu/geoserver/nebih/wms?",
+  {
+    layers: "KUL_RESZLET_VW",
+    styles: "vedettseg",
+    transparent: true,
+    format: "image/png",
+    opacity: 0.7,
+    attribution:
+      'Erdők védettsége &copy; <a href="https://erdoterkep.nebih.gov.hu/">NÉBIH</a>',
   }
 );
 

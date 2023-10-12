bbb64fa9f0c358d4fa3cf8f7022610f2ad6339c3
andrashann <andrashann@users.noreply.github.com>
2023-10-12 18:08:50 +0200

    removed mapy layer, added protected areas layer, code formatting

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ This document assumes an understanding of geocaching in general and the above-me
 - A button with the caption `⤓` is added to the bottom toolbar. When clicked, lines connecting the stages of multi-caches are downloaded as a GPX file. [0.1.0]
 - A button with the caption `ᓬ` is added to the bottom toolbar. When clicked, the user is prompted to select a GPX file. Then, the user can select a color. The tracks and waypoints in the file are added to the map in the selected color. _This functionality is adapted from [this repository](https://github.com/andrashann/geomap-add-gpx)._ [0.1.0]
 - Added [WaymarkedTrails](https://hiking.waymarkedtrails.org) overlay layer (choice is not stored when page is reloaded) [0.1.0]
-- Added [Mapy Base](https://en.mapy.cz) base layer (choice is not stored when page is reloaded) [0.1.0]
+- Added overlay map of protected areas (source: [Természetvédelmi Információs Rendszer](https://web.okir.hu/map/?config=TIR&lang=hu); choice is not stored when page is reloaded) [0.2.0]
--- gm.js
+++ gm.js
@@ -1,28 +1,50 @@
 //load required js packages
-document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src =
-  'https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.21.1/parser.min.js';
-document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src =
-  'https://cdn.jsdelivr.net/npm/togpx@0.5.4/togpx.js'
+document
+  .getElementsByTagName("head")[0]
+  .appendChild(document.createElement("script")).src =
+  "https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/3.21.1/parser.min.js";
+document
+  .getElementsByTagName("head")[0]
+  .appendChild(document.createElement("script")).src =
+  "https://cdn.jsdelivr.net/npm/togpx@0.5.4/togpx.js";
 
 // buttons to add to the map
 const newButtons = [
-  { id: 'mpts', class: 'mpts', title: 'Multipontok összekötése', onClick: 'joinMultiPoints()', textContent: '☍' },
-  { id: 'dlmpts', class: 'dlmpts', title: 'Összekötés letöltése GPX-ként', onClick: 'dlMultiPointJoins()', textContent: '⤓' },
-  { id: 'lgpx', class: 'lgpx', title: 'GPX file betöltése', onClick: 'loadGPX()', textContent: 'ᓬ' },
-]
-buttonsDiv = document.getElementById('setup')
+  {
+    id: "mpts",
+    class: "mpts",
+    title: "Multipontok összekötése",
+    onClick: "joinMultiPoints()",
+    textContent: "☍",
+  },
+  {
+    id: "dlmpts",
+    class: "dlmpts",
+    title: "Összekötés letöltése GPX-ként",
+    onClick: "dlMultiPointJoins()",
+    textContent: "⤓",
+  },
+  {
+    id: "lgpx",
+    class: "lgpx",
+    title: "GPX file betöltése",
+    onClick: "loadGPX()",
+    textContent: "ᓬ",
+  },
+];
+buttonsDiv = document.getElementById("setup");
 for (const btn of newButtons) {
   const div = document.createElement("div");
-  for (const attr of ['id', 'class', 'title', 'onClick']) {
-    div.setAttribute(attr, btn[attr])
+  for (const attr of ["id", "class", "title", "onClick"]) {
+    div.setAttribute(attr, btn[attr]);
   }
-  div.textContent = btn.textContent
+  div.textContent = btn.textContent;
   buttonsDiv.appendChild(div);
 }
 
 // load the script functionality into the page
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('gm_functions.js');
+var s = document.createElement("script");
+s.src = chrome.runtime.getURL("gm_functions.js");
 s.onload = function () {
   this.remove();
 };
@@ -30,8 +52,8 @@ s.onload = function () {
 (document.head || document.documentElement).appendChild(s);
 
 // load the script functionality into the page
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('gm_others.js');
+var s = document.createElement("script");
+s.src = chrome.runtime.getURL("gm_others.js");
 s.onload = function () {
   this.remove();
 };
